cb18ee33db74294c8dd48fe17b6c87e84e7c4c36
MJ <abc@hotmail.com>
2023-10-03 11:53:50 +0800

    add exp1 and exp2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
+function Experience1() {
+  return (
+    <>
+      <h3>Moglix, Pune</h3>
+      <h4>Front End Developer</h4>
+      <ul>
+        <li>Manged Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus, eius!</li>
+      </ul>
+    </>
+  )
+}
+
+function Experience2() {
+  return (
+    <>
+      <h3>Sigma School</h3>
+      <h4>Front End Developer</h4>
+      <ul>
+        <li>Manged Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus, eius!</li>
+      </ul>
+    </>
+  )
+}
+
+
+
 function App() {
   return (
     <>
       <h1>MJ</h1>
       <h2>Software Developer</h2>
+      <Experience1></Experience1>
+      <Experience2></Experience2>
     </>
   )
 }
